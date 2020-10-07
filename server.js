9c9d7780b47884c517f4e5a2806b5e46771465be
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-07 18:14:45 -0500

    created new page and partial view to be resuable

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const articles = [{
+        title: 'testt',
+        createdAt: new Date(),
+        description: 'testd'
+    }]
+    res.render('articles/index', {articles: articles})
 })
 
 app.listen(5000)

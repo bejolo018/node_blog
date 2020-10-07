e06e9f41880f825b4807742c7a73152d98caf428
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-07 18:25:52 -0500

    Created model, connected to db, created post route for articles

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const articleRouter = require('./routes/articles')
 
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true})
+
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     const articles = [{
@@ -14,5 +17,7 @@ app.get('/', (req, res) => {
     }]
     res.render('articles/index', {articles: articles})
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)

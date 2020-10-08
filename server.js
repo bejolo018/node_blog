dc751a9cfa580b2ba2f33a041a58ae2dd04b4a7f
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-07 19:44:50 -0500

    implemented delete functionality and used dompurify to sanitize HTML so that no code can be injected by would be wrongdoers

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose')
 const Article = require('./models/article')
 const app = express()
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 require('dotenv/config')
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 
 app.set('view engine', 'ejs')
-
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({
